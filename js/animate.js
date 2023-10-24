223dd394d64a5dc1a79f7eed959b6abcd18dd0f9
Vinicius Rodrigues <vinizika508@gmail.com>
2023-10-24 18:06:31 -0300

    uo

---
+++ js/animate.js
@@ -0,0 +1,9 @@
+$('.nav a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100
+	}, 500);
+});
---
+++ js/value.js
@@ -0,0 +1,12 @@
+  const menuIcon = document.getElementById("menu-icon");
+  const menu = document.getElementById("menu");
+
+  menuIcon.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+
+  fetch('https://bot.sshtproject.com/get_plains')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("conteiner").innerHTML = data;
+    }).catch(err => { return });
