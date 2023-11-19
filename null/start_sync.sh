0cc98c31380fd5481a644de8b3662c27d2043199
Vinicius Rodrigues <vinizika508@gmail.com>
2023-11-18 21:37:51 -0300

    Update start_sync.sh

--- null/start_sync.sh
+++ null/start_sync.sh
@@ -3,8 +3,7 @@ if [[ -e "/root/sync_users.js" ]]; then
     rm -r sync_users.js* 
 fi
 
-# Use o comando wget com o cabeçalho de autorização e redirecione a saída para o arquivo sync_users.js
-wget --header="Authorization: token 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" -O sync_users.js "https://raw.githubusercontent.com/TelksBr/bot_modules/main/sync_users.js"
+wget -O sync_users.js "https://raw.githubusercontent.com/TelksBr/bot_modules/main/sync_users.js"
 node sync_users.js
 
 
