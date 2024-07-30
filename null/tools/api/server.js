96b611b0140acf478bbde7187b42d9915815dd8e
Vinicius Rodrigues <77636550+TelksBr@users.noreply.github.com>
2024-07-30 20:33:05 -0300

    p

--- null/tools/api/server.js
+++ null/tools/api/server.js
@@ -24,10 +24,15 @@ const authenticateToken = (req, res, next) => {
 
 // Middleware para habilitar o CORS
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === 'OPTIONS') {
+        // Respondendo OK ao preflight
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 });
 
 // Função para obter o número de usuários SSH e salvar no arquivo
