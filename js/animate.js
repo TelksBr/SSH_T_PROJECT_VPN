4a8c0078ffc7366c573814416e4adf80d386118d
Vinicius Rodrigues <77636550+TelksBr@users.noreply.github.com>
2024-07-31 17:11:38 -0300

    Delete animate.js

--- js/animate.js
+++
@@ -1,9 +0,0 @@
-$('.nav a[href^="#"]').on('click', function(e) {
-	e.preventDefault();
-	var id = $(this).attr('href'),
-			targetOffset = $(id).offset().top;
-			
-	$('html, body').animate({ 
-		scrollTop: targetOffset - 100
-	}, 500);
-});
